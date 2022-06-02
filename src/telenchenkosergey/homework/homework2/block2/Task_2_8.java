fee414f1161c90a262bb113dd7fdfb3590de43e9
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-06-02 10:15:03 +0300

    added printing initial triangles

--- src/telenchenkosergey/homework/homework2/block2/Task_2_8.java
+++ src/telenchenkosergey/homework/homework2/block2/Task_2_8.java
@@ -3,6 +3,7 @@ package telenchenkosergey.homework.homework2.block2;
 public class Task_2_8 {
     public static void main(String[] args) {
         String star = "*";
+        // String empty = " ";
 
         int apex = 1;
         int base = 4;
@@ -16,5 +17,19 @@ public class Task_2_8 {
         for (int j = base; j >= apex; j--) {
             System.out.println(star.repeat(j));
         }
+
+        // for (int i = apex; i <= base; i++) {
+        //     System.out.print(empty.repeat(base - apex));
+        //     System.out.println(star.repeat(apex));
+        //     apex++;
+        // }
+
+        // System.out.println("\n");
+
+        // for (int i = 0; i <= base; i++) {
+        //     System.out.print(empty.repeat(i));
+        //     System.out.println(star.repeat(base - i));  
+        // } 
     }
+
 }
