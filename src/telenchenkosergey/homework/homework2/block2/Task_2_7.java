13e504849ef24de553a06d3ef23066da3c88376b
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-06-06 06:46:16 +0300

    refactor tasks 1.2, 2.5-2.7 hw2

--- src/telenchenkosergey/homework/homework2/block2/Task_2_7.java
+++ src/telenchenkosergey/homework/homework2/block2/Task_2_7.java
@@ -4,10 +4,8 @@ public class Task_2_7 {
     public static void main(String[] args) {
         int sum = 0;
 
-        for (int i = 1; i < 100; i++) {
-            if (i % 2 != 0) {
-                sum += i;
-            }
+        for (int i = 1; i < 100; i+=2) {
+            sum += i;
         }
         System.out.println(sum);
     }
