2e8752a3c79bae0c8b14d00e6d3805276f1b8d75
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-06-09 13:49:01 +0300

    modified classwork

--- src/telenchenkosergey/homework/classwork/Classwork.java
+++ src/telenchenkosergey/homework/classwork/Classwork.java
@@ -1,38 +1,47 @@
 package telenchenkosergey.homework.classwork;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Classwork {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // String str = "London is the capital of Great Britain";
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append("london");
+        System.out.println(strBuilder.reverse().toString());
 
-        int size = 5;
-        String[] strings = new String[5];
 
-        for (int i = 0; i < size; i++) {
-            String s = input.nextLine();
-            strings[i] = s;
-        }
+        // for (int i = 0; i < str.length(); i++) {
+        //     System.out.println(strArray[i]);
+        // }
+        // Scanner input = new Scanner(System.in);
 
-        input.close();
+        // int size = 5;
+        // String[] strings = new String[5];
 
-        String min = strings[0];
-        String max = strings[0];
+        // for (int i = 0; i < size; i++) {
+        //     String s = input.nextLine();
+        //     strings[i] = s;
+        // }
 
-        for (int i = 1; i < strings.length; i++) {
-            if (strings[i].length() < min.length()) {
-                min = strings[i];
-            }
-        }
+        // input.close();
 
-        for (int i = 1; i < strings.length; i++) {
-            if (strings[i].length() > max.length()) {
-                max = strings[i];
-            }
-        }
+        // String min = strings[0];
+        // String max = strings[0];
 
-        System.out.println(min);
-        System.out.println(max);
+        // for (int i = 1; i < strings.length; i++) {
+        //     if (strings[i].length() < min.length()) {
+        //         min = strings[i];
+        //     }
+        // }
+
+        // for (int i = 1; i < strings.length; i++) {
+        //     if (strings[i].length() > max.length()) {
+        //         max = strings[i];
+        //     }
+        // }
+
+        // System.out.println(min);
+        // System.out.println(max);
         // System.out.println(myPhone.getManufacturer());
         // System.out.println(myPhone.getModel());
         // System.out.println(Phone.call("+375293769611"));
