7aaf691208bd24363d63313b05fd5a1bf6750d2b
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-06-13 23:38:23 +0300

    initial commit for hw5

---
+++ src/telenchenkosergey/homework/homework5/money/Main.java
@@ -0,0 +1,19 @@
+package telenchenkosergey.homework.homework5.money;
+
+public class Main {
+    public static void main(String[] args) {
+        Money money1 = new Money(124L, 0);
+        Money money2 = new Money(124L, 0);
+
+        Money sumMoney = money1.sum(money2);
+        System.out.println(sumMoney);
+
+        Money subtractMoney = money1.subtract(money2);
+        System.out.println(subtractMoney);
+
+        Money divideMoney = money1.divide(money2);
+        System.out.println(divideMoney);
+
+        System.out.println(money1.equality(money2));
+    }
+}
