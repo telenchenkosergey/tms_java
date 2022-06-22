b90c95efcbc865806c4cd19d2d1c89ad865b1568
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-06-22 18:59:34 +0300

    override hw5

--- src/telenchenkosergey/homework/homework6/exception/Main.java
+++ src/telenchenkosergey/homework/homework6/exception/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Car toyota = new Car("Toyota Auris", 160, 5000);
-        Car mazda = new Car("Mazda 6", 150, 6000);
+        Car toyota = new Car("Toyota Auris", 200, 5000);
+        Car mazda = new Car("Mazda 6", 220, 6000);
 
         toyota.start();
         mazda.start();
