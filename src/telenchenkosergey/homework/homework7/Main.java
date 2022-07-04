540eef2543bfc5068ef71b332ce28b9a9250bd68
Sergey Telenchenko <telenchenko1988@gmail.com>
2022-07-04 19:28:12 +0300

    added comments

--- src/telenchenkosergey/homework/homework7/Main.java
+++ src/telenchenkosergey/homework/homework7/Main.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        // Book Objects
         Book king = new Book("S. King", "Misery");
         Book orwell = new Book("G. Orwell", "Animal Farm");
         Book schildt = new Book("H. Schildt", "Java: A Beginner's Guide");
         Book galata = new Book("I. Galata", "Kotlin Apprentice");
         Book bayliss = new Book("D. Bayliss", "Android Apprentice");
 
+        // Arraylist of returned books
         ArrayList<Book> returnedBooks = new ArrayList<>();
         returnedBooks.add(king);
         returnedBooks.add(schildt);
 
+        // Arraylist of Readers
         ArrayList<Reader> readers = new ArrayList<>();
 
         readers.add(new Reader("Sergey Telenchenko"));
