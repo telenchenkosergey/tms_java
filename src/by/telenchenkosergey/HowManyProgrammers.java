7481cd64f00af8f68f5c02c60b96a75e8a26ceff
Sergey Telenchenko <telenchenko1988@gmail.com>
2024-07-17 20:20:08 +0300

    Add HowManyProgrammers Task

--- src/by/telenchenkosergey/HowManyProgrammers.java
+++ src/by/telenchenkosergey/HowManyProgrammers.java
@@ -2,6 +2,20 @@ package by.telenchenkosergey;
 
 public class HowManyProgrammers {
     public static void main(String[] args) {
-        
+        int programmers = 0;
+        System.out.println(programmers + " программист" + howMany(programmers, "", "а", "ов"));
+    }
+
+    public static String howMany(int count, String one, String few, String many) {
+        if (count % 100 >= 11 && count % 100 <= 14) {
+            return many;
+        }
+        if (count % 10 == 1 && count % 100 != 11) {
+            return one;
+        } else if (count % 10 >= 2 && count % 10 <= 4) {
+            return few;
+        } else {
+            return many;
+        }
     }
 }
